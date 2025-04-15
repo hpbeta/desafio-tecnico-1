051f186609b3091a42ec16dc87d80a7f84affcdc
hpbeta <hpbeta16.l@gmail.com>
2025-04-14 22:37:06 -0300

    Diminuindo a largura do cards no carrossel

--- src/products.js
+++ src/products.js
@@ -63,4 +63,20 @@ export const products = [
     offer: "10% OFF",
     installments: "Ou em até 10x de R$ 7,90",
   },
+  {
+    image: "./src/assets/model.png",
+    name: " Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+    price: "R$ 100,00",
+    discount: "R$ 79,90",
+    offer: "10% OFF",
+    installments: "Ou em até 10x de R$ 7,90",
+  },
+  {
+    image: "./src/assets/model.png",
+    name: " Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+    price: "R$ 100,00",
+    discount: "R$ 79,90",
+    offer: "10% OFF",
+    installments: "Ou em até 10x de R$ 7,90",
+  },
 ];
